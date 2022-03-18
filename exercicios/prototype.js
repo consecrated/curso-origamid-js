8d7cc8a1d3da19d4eeba6bda093e6777d1c502ad
Rafael Martins <contato@inovarestudio.com.br>
2022-03-18 15:06:18 -0300

    finalizando atividades de prototype

--- exercicios/prototype.js
+++ exercicios/prototype.js
@@ -3,20 +3,40 @@
 // Crie um método no protótipo que retorne
 // o nome completo da pessoa
 
+function Pessoa (nome, sobrenome, idade) {
+    this.nome = nome;
+    this.sobrenome = sobrenome;
+    this.idade = idade;
+}
+
+Pessoa.prototype.nomeCompleto = function () {
+    return `${this.nome} ${this.sobrenome}`;
+}
+
+const user = new Pessoa('Rafael Henrique', 'Martins', 22);
+console.log(user)
+console.log(user.nomeCompleto());
+
+
 // Liste os métodos acessados por 
 // dados criados com NodeList,
 // HTMLCollection, Document
 
+Object.getOwnPropertyNames(NodeList.prototype);
+Object.getOwnPropertyNames(HTMLCollection.prototype);
+Object.getOwnPropertyNames(Document.prototype);
+
+
 // Liste os construtores dos dados abaixo
 const li = document.querySelector('li');
 
-li;
-li.click;
-li.innerText;
-li.value;
-li.hidden;
-li.offsetLeft;
-li.click();
+li; // HTMLEllement
+li.click; // Function
+li.innerText; // String
+li.value; // Number
+li.hidden; // Boolean
+li.offsetLeft; // Number
+li.click(); // undefined
 
 // Qual o construtor do dado abaixo:
-li.hidden.constructor.name;
+li.hidden.constructor.name; // String
