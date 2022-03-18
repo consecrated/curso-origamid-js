85569d48964de4f12aace98a9f05cfae8c159fb8
Rafael Martins <contato@inovarestudio.com.br>
2022-03-18 12:06:55 -0300

    exercícios sobre constructor function

--- main.js
+++ main.js
@@ -1,19 +1,48 @@
-function Dom (seletor) {
-    this.element = function () {
-        return document.querySelector(seletor);
-    }
-    this.active = function (classe) {
-        this.element().classList.add(classe);
+// Transforme o objeto abaixo em uma Constructor Function
+const pessoa = {
+    nome: 'Nome pessoa',
+    idade: 0,
+    andar() {
+        console.log(this.nome + ' andou');
     }
 }
 
+function Pessoa (nome, idade) {
+    this.nome = nome;
+    this.idade = idade;
+    this.andar = function () {
+        console.log(this.nome + 'andou');
+    }
+}
+  
+// Crie 3 pessoas, João - 20 anos,
+// Maria - 25 anos, Bruno - 15 anos
 
-function click () {
-    const classList = new Dom('li');
-    classList.active('noia');
+const joao = new Pessoa('João', 20);
+const maria = new Pessoa('Maria', 25);
+const bruno = new Pessoa('Bruno', 15);
+  
+  
+// Crie uma Constructor Function (Dom) para manipulação
+// de listas de elementos do dom. Deve conter as seguintes
+// propriedades e métodos:
+//
+// elements, retorna NodeList com os elementos selecionados
+// addClass(classe), adiciona a classe a todos os elementos
+// removeClass(classe), remove a classe a todos os elementos
 
-
+function Dom (selector) {
+    const elementsList = document.querySelectorAll(selector);
+    this.elements = elementsList;
+    this.addClass = function (classes) {
+        elementsList.forEach((item, index) => {
+            item.classList.add(classes);
+        });
+    }
 }
 
+const element = new Dom('li');
+element.addClass('selected');
 
-document.querySelector('li').addEventListener('click', click);
+
+
